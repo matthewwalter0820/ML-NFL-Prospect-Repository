9c62e133386bbd9ef51b28f897cf7b4102b36052
Matthew Walter <matthewwalter0820@gmail.com>
2025-04-20 18:48:29 -0400

    Add files via upload

---
+++ Model_Update.R
@@ -0,0 +1,72 @@
+[33mcommit ab068c2440f728c4a63954c7effa125e21980d9e[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mplease-add-file[m[33m, [m[1;32mtrunc-code[m[33m, [m[1;32madd-file[m[33m)[m
+Merge: 5f0151d 6185ce0
+Author: Matthew Walter <matthewwalter0820@gmail.com>
+Date:   Sun Apr 20 18:40:21 2025 -0400
+
+    Truncated Code
+
+[33mcommit 5f0151d0a6181cdf74153f15fd6035cde2cf37eb[m
+Author: Matthew Walter <matthewwalter0820@gmail.com>
+Date:   Sun Apr 20 18:36:40 2025 -0400
+
+    Added truncated version of Model.R
+
+[33mcommit 8aaeeab1970032285753ef429f571263384b566c[m[33m ([m[1;32mmodel-update[m[33m, [m[1;32mmain[m[33m)[m
+Author: Matthew Walter <matthewwalter0820@gmail.com>
+Date:   Sun Apr 20 18:29:42 2025 -0400
+
+    Added truncated version of Model.R
+
+[33mcommit 6185ce0761e8cf1b8ea10eea4d4d0f97975cdf1c[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Merge: 745cc4a 59fc88e
+Author: matthewwalter0820 <matthewwalter0820@gmail.com>
+Date:   Sun Apr 20 18:12:56 2025 -0400
+
+    Merge pull request #4 from matthewwalter0820/matthewwalter0820-patch-1
+    
+    Update README.txt
+
+[33mcommit 59fc88edbf1ef42c8b4bbd506dc09c1bb9205762[m[33m ([m[1;31morigin/matthewwalter0820-patch-1[m[33m)[m
+Author: matthewwalter0820 <matthewwalter0820@gmail.com>
+Date:   Sun Apr 20 18:12:16 2025 -0400
+
+    Update README.txt
+    
+    Added steps to README
+
+[33mcommit 745cc4a79abbe20a8e6e022e4ed84fd34cfa7309[m
+Author: Rank7Kaiju <connorblunt01@gmail.com>
+Date:   Sun Apr 20 17:42:07 2025 -0400
+
+    Some filepaths fixed
+    
+    Got the code to run through the filesi n github correctly. Should be able to run source on Model.R and be good until the 2024 files.
+
+[33mcommit ef6294d2939312dd6d14c886de77772512012167[m
+Merge: 2122bd6 588ebf9
+Author: Muhammad Ali Asjad <asjadmi@mail.uc.edu>
+Date:   Sat Apr 19 21:25:13 2025 -0400
+
+    Merge pull request #3 from matthewwalter0820/shiny
+    
+    DT Shiny App
+
+[33mcommit 2122bd64eb8dab436a7da9e432808aa33a335a7b[m
+Author: Rank7Kaiju <connorblunt01@gmail.com>
+Date:   Sat Apr 19 17:58:23 2025 -0400
+
+    Added README
+    
+    Added a README file to the system. Need the shiny app merged in order to finish writing the file.
+
+[33mcommit 588ebf9b032fd58573f32b2aebf4ebeb59dbd68e[m[33m ([m[1;31morigin/shiny[m[33m)[m
+Author: delaetjr <jdelaet123@gmail.com>
+Date:   Sat Apr 19 15:05:36 2025 -0400
+
+    DT Shiny App
+
+[33mcommit 41b7a4c79e845fbf7c77042775280d26403f27a6[m
+Author: Matthew Walter <matthewwalter0820@gmail.com>
+Date:   Thu Apr 17 21:10:24 2025 -0400
+
+    Adding defensive files
